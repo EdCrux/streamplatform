fbdc51c8ad081667ea6d04afcb3c02761416e0be
Eduardo Cruz <jecruxiz@gmail.com>
2024-11-05 15:36:52 -0600

    proto update

--- common/run.sh
+++ common/run.sh
@@ -5,8 +5,8 @@ RELEASE_VERSION=$2
 
 #sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+#go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+#go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
 protoc \
     --go_out=./golang \
